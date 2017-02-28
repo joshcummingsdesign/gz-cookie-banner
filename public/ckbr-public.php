ed68aeb040c62bb335646a5e2b73f2e3d7b26655
josh <josh@madebygrizzly.com>
2017-02-27 18:01:55 -0800

    Enqueues scripts in correct hook

--- public/ckbr-public.php
+++ public/ckbr-public.php
@@ -1,14 +1,23 @@
 <?php
 
+/*=============================================
+=            Enqueue Public Assets            =
+=============================================*/
+
+function ckbr_display_public_assets()
+{
+    wp_enqueue_style('ckbr_public_styles', plugins_url('css/ckbr-public-styles.css', __FILE__));
+    wp_enqueue_script('ckbr_public_script', plugins_url('js/ckbr-public-script.js', __FILE__), ['jquery'], null, true);
+}
+add_action('wp_enqueue_scripts', 'ckbr_display_public_assets');
+
+
 /*============================================
 =           Display Public Content           =
 ============================================*/
 
 function ckbr_display_public_content()
 {
-    wp_enqueue_style('ckbr_public_styles', plugins_url('css/ckbr-public-styles.css', __FILE__));
-    wp_enqueue_script('ckbr_public_script', plugins_url('js/ckbr-public-script.js', __FILE__), ['jquery'], null, true);
-
     $settings = get_option('ckbr_settings');
 
     wp_localize_script('ckbr_public_script', 'settings', [
